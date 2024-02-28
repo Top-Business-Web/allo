8b2ff82ecbb7bf0fdaca6e58f45bf5e05790571b
eldapour <abdallahmahmoud1422003@gmail.com>
2024-02-28 17:06:34 +0200

    Use Git LFS for large file

--- resources/views/site/partner_success.blade.php
+++ resources/views/site/partner_success.blade.php
@@ -22,7 +22,7 @@
     <div class="partner-area section-padding">
         <div class="container">
             <div class="section-title">
-                <h6>{{ trans('site.distrubtion') }}</h6>
+                <h6>{{ trans('site.partners') }}</h6>
                 <h2>{{ trans('site.partner_head')}}</h2>
             </div>
             <div class="row">
